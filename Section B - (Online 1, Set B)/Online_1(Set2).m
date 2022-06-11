d3a6e6404299d77cb0948db392543ceabda6572a
Misty Samia <62679682+mistysamia@users.noreply.github.com>
2022-06-12 04:10:28 +0600

    Add files via upload

---
+++ Section B - (Online 1, Set B)/Online_1(Set2).m
@@ -0,0 +1,38 @@
+%INPUT
+img1 = imread('image2.png');
+img2 = imread('image1.png');
+
+%No of stripes and stripe size declaration
+stripe = 6;
+size = 100;
+customSize = stripe * size;
+
+%Resizing Images
+img1 = imresize(img1, [customSize customSize]);
+img2 = imresize(img2, [customSize customSize]);
+
+% Concate and Stripe the both images
+tempImage = img1;
+j=1;
+for i = customSize/2 : 1 : customSize
+    tempImage( 1:1:j,j, :) = img2(j ,customSize/2:1:i, :);
+    j=j+1;
+end
+j=customSize/2+1;
+k=customSize;
+for i = customSize/2 : 1 : customSize
+    tempImage( 1:1:j,i, :) = img2(i ,customSize/2:1:k, :);
+    j=j-1;
+    k=k-1;
+end
+
+%Display Images
+figure;
+imshow(img1);
+figure;
+imshow(img2);
+figure;
+imshow(tempImage);
+
+% Save the New Image
+imwrite(tempImage, 'output.jpg');
